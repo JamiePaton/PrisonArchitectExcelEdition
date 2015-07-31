e7c21326289b86b3b1f0e5486dd28cb4f1e40e96
JamiePaton <JamiePaton@users.noreply.github.com>
2015-07-31 22:02:37 +0100

    reads colours from excel file

--- excelreader.py
+++ excelreader.py
@@ -16,12 +16,40 @@ import hypothesis as hs
 import openpyxl
 import xlwings
 import os
-
+import pprint
+import paformat
 
 def main(args):
+    logger = logging.getLogger(__name__)
+    logger.info('opening excel document')
     wb = xlwings.Workbook(os.path.join(os.getcwd(), 'prisontest.xlsx'), app_visible=False)
-    print xlwings.Range('Small', 'E2').color
+    
+    colours = build_material_dict()
+    materials = build_material_array()
+    
+#    print colours['(118, 147, 60)']
+    logger.info('closing excel document')
     wb.close()
+
+def build_material_array():
+    pass
+
+def build_material_dict():
+    material_colours = {}
+    n=1
+    while True:
+        material = xlwings.Range('Settings', 'A{}'.format(n)).value
+        logger.debug('A{}:\t{}'.format(n, material))
+        if material is None:
+            break
+        else:
+            colour = xlwings.Range('Settings', 'B{}'.format(n)).color
+            logger.debug('B{}:\t{}'.format(n, str(colour)))
+            material_colours[str(colour)] = material
+            n += 1
+    return material_colours
+    
+    
 class Testing(unittest.TestCase):
     """
     
@@ -39,7 +67,7 @@ class Testing(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
     logger = logging.getLogger(__name__)
     logger.info(''.join([TITLE, ' v', VERSION, ' ', AUTHOR]))
     sys.exit(main(sys.argv))
